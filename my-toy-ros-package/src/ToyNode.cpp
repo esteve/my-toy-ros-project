0875447e7032c5211bae64fb039007c5d30c42a7
Esteve Fernandez <esteve@apache.org>
2026-01-22 20:00:47 +0100

    feat!: add a multiply function
    Signed-off-by: Esteve Fernandez <esteve@apache.org>

--- my-toy-ros-package/src/ToyNode.cpp
+++ my-toy-ros-package/src/ToyNode.cpp
@@ -5,8 +5,14 @@ int sum(int x, int y)
   return x + y;
 }
 
+int multiply(int x, int y)
+{
+  return x * y;
+}
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char ** argv)
 {
-  int result = sum(41, 1);
-  printf("hello world my-toy-ros-package package: %d\n", result);
+  int result_sum = sum(41, 1);
+  int result_multiply = multiply(result_sum, 2);
+  printf("hello world my-toy-ros-package package: %d\n", result_multiply);
 }
